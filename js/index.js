394031b435ace936d8047c92812c82eb5c8a9073
Shane Geary <sthomas88@live.com>
2022-04-29 16:59:22 -0700

    created paddle, and movement

--- js/index.js
+++ js/index.js
@@ -9,35 +9,97 @@ let x = canv.width/2;
 let y = canv.height-30;
 
 // creating variables for movement for ball
-let mx = 2
-let my = -2
+let mx = 1
+let my = -1
+
+// creating a variable for ball radius
+let ballRadius = 5;
+
+// creating variable for paddle paddleX starting locations for paddle
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canv.width-paddleWidth)/2;
+
+// creating variables for left and right key presses, is false because key are not pressed in the beginning
+let rightKey = false;
+let leftKey = false;
+
+// adding event listeners for key presses
+document.addEventListener('keydown', keyDown, false);
+document.addEventListener('keyup', keyUp, false);
 
 // create function to draw circle
 function drawBall() {
     render.beginPath();
-    render.arc(x, y, 10, 0, Math.PI*2);
+    render.arc(x, y, ballRadius, 0, Math.PI*2);
     render.fillStyle = 'blue';
     render.fill();
     render.closePath();
+}
+
+function drawPaddle() {
+    render.beginPath();
+    render.rect(paddleX, canv.height-paddleHeight, paddleWidth, paddleHeight);
+    render.fillStyle = 'blue';
+    render.fill();
+    render.closePath;
    
 }
 
 function draw() {
     render.clearRect(0, 0, canv.width, canv.height);
     drawBall();
-     // added variables to move circle
-     x += mx;
-     y += my;
+    drawPaddle();
+    // hit detection on walls
+    if (y + my > canv.height-ballRadius || y + my < ballRadius) {
+        my = -my;
+    }
+    if ( x + mx > canv.width-ballRadius || x + mx < ballRadius) {
+        mx = -mx;
+    }
+    
+    // paddle speed and keeping paddle within width
+    if (rightKey) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canv.width) {
+            paddleX = canv.width - paddleWidth;
+        }
+    }
+    else if(leftKey) {
+        paddleX -= 7;
+        if (paddleX < 0) {
+            paddleX = 0;
+        }
+    }
+    // added variables to move circle
+    x += mx;
+    y += my;
 }
+
+// creating functions for key down and key up
+function keyDown(k) {
+    if (k.key == 'Right' || k.key == 'ArrowRight') {
+        rightKey = true;
+    }
+    else if (k.key == 'Left' || k.key == 'ArrowLeft') {
+        leftKey = true;
+    }
+}
+
+    function keyUp(k) {
+        if (k.key == 'Right' || k.key == 'ArrowRight') {
+            rightKey = false;
+        }
+        else if(k.key = 'Left' || k.key == 'ArrowLeft') {
+            leftKey = false;
+        }
+    }
+
 setInterval(draw, 10);
 
-
-// need to create ball
 // need to cread bottom paddle
 // need to create bricks
 // need to create movement for bottom paddle
-// need to create action that moves ball
-// hit detection
 // removing blocks on hit detection
 
 
